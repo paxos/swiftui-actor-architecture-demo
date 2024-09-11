60d440e73ca20adeaf72bb94b30413f03c115290
Patrick Dinger <121539+paxos@users.noreply.github.com>
2024-09-11 10:16:55 -0700

    Initial Commit

---
+++ SwiftUIActorArchitecture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIActorArchitecture
+//
+//  Created by Patrick Dinger on 9/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIActorArchitecture/SwiftUIActorArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIActorArchitectureApp.swift
+//  SwiftUIActorArchitecture
+//
+//  Created by Patrick Dinger on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIActorArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
